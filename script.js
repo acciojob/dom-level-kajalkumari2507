6ff8e081aa43f9e09cc05d6bedfdb8677fb8a7a2
Kajal Kumari <114283323+kajalkumari2507@users.noreply.github.com>
2023-06-30 20:57:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+        const levelElement = document.getElementById("level");
+        let level = 0;
+
+        let parent = levelElement.parentElement;
+        while (parent !== null) {
+          level++;
+          parent = parent.parentElement;
+        }
+
+        alert("The level of the element is: " + level);
+      });
